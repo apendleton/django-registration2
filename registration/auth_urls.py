3d295478dd80f60976b19e8a1b65b3644c68ad1c
Andrew Pendleton <apendleton@sunlightfoundation.com>
2015-05-19 17:39:32 -0400

    Switch to base64 encoding for password reset to match Django 1.6+

--- registration/auth_urls.py
+++ registration/auth_urls.py
@@ -12,7 +12,7 @@ urlpatterns = patterns('django.contrib.auth.views',
     url(r'^password/reset/$', 'password_reset',
         name='password-reset'),
 
-    url(r'^password/reset/(?P<uidb36>[0-9A-Za-z]{1,13})-(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+    url(r'^password/reset/(?P<uidb64>[0-9A-Za-z]{1,13})-(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
         'password_reset_confirm', name='password-reset-confirm'),
 
     url(r'^password/reset/complete/$', 'password_reset_complete',
